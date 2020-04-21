5aa73f7bf7dc986acab37fcdfe6720cfca955913
sharif.ny <sharifaiub09@gmail.com>
2020-04-20 22:16:13 -0400

    Commit 1

--- src/testBase/excelFile/CalculateNumbersFromExcel.java
+++ src/testBase/excelFile/CalculateNumbersFromExcel.java
@@ -4,8 +4,10 @@ import org.apache.poi.xssf.usermodel.XSSFCell;
 import org.apache.poi.xssf.usermodel.XSSFRow;
 import org.apache.poi.xssf.usermodel.XSSFSheet;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
 
 import java.io.FileInputStream;
 import java.io.IOException;
@@ -16,9 +18,9 @@ public class CalculateNumbersFromExcel {
         System.setProperty("webdriver.chrome.driver",System.getProperty("user.dir")+"\\src\\drivers\\chromedriver.exe");
         System.setProperty("webdriver.chrome.silentOutput","true");//it's prevent to showing unnecessary logs from the browser site in the console
         WebDriver driver = new ChromeDriver();
-        driver.get("http://newtours.demoaut.com/mercurywelcome.php");
+        driver.get("https://www.moneycontrol.com/fixed-income/calculator/yes-bank/fixed-deposit-calculator-YB-BYB001.html?classic=true");
 
-        Thread.sleep(5000);
+        Thread.sleep(2000);
         //create an object to read data from excel for FileInputStream class
         FileInputStream file = new FileInputStream("C:\\Users\\sharif.ny\\IdeaProjects\\SeleniumProjectWithOutMaven\\src\\T4.xlsx");
 
@@ -30,7 +32,7 @@ public class CalculateNumbersFromExcel {
 
         int rowCount = sheet.getLastRowNum();//return total row numbers
 
-        for (int row=0;row<rowCount;row++){
+        for (int row=1;row<rowCount;row++){
             XSSFRow currentRow = sheet.getRow(row);
 
             //we can read cells value either directly like bellow code or
@@ -40,29 +42,44 @@ public class CalculateNumbersFromExcel {
             XSSFCell principle = currentRow.getCell(0);
             int princ = (int)principle.getNumericCellValue();
 
+
             XSSFCell roi = currentRow.getCell(1);
             int rateOfInterest = (int)roi.getNumericCellValue();
 
-            XSSFCell Frequency = currentRow.getCell(2);
+            XSSFCell period = currentRow.getCell(2);
+            int per = (int)period.getNumericCellValue();
+
+
+            XSSFCell Frequency = currentRow.getCell(3);
             String freq = Frequency.getStringCellValue();
 
-            XSSFCell maturity = currentRow.getCell(3);
-            int mat = (int)maturity.getNumericCellValue();
+            XSSFCell maturity = currentRow.getCell(4);
+            int ExpectMat = (int)maturity.getNumericCellValue();
 
+            driver.findElement(By.id("principal")).sendKeys(String.valueOf(princ));
+            driver.findElement(By.id("interest")).sendKeys(String.valueOf(rateOfInterest));
+            driver.findElement(By.id("tenure")).sendKeys(String.valueOf(per));
 
+            Select select = new Select(driver.findElement(By.id("tenurePeriod")));
+            select.selectByVisibleText("year(s)");
 
+            Select select1 = new Select(driver.findElement(By.id("frequency")));
+            select1.selectByVisibleText(freq);
 
+            driver.findElement(By.xpath(".//*[@id='fdMatVal']/div[2]/a[1]/img")).click();
 
+            String actualVal = driver.findElement((By.xpath(".//*[@id='resp_matval']/strong"))).getText();
 
+            if ((Double.parseDouble(actualVal))==ExpectMat){
+                System.out.println("TEST PASSED");
 
-
+            }else {
+                System.out.println("Test Failed");
+            }
+            driver.findElement(By.xpath(".//*[@id='fdMatVal']/div[2]/a[2]/img")).click();
 
         }
-
-
-
-
-
+        driver.close();
 
     }
 }
