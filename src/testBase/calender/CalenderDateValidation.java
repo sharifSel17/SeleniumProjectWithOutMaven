1baa79f069924b5728295e12ad1ab2113a227d7d
sharif.ny <sharifaiub09@gmail.com>
2020-04-21 21:00:12 -0400

    Commit 1

---
+++ src/testBase/calender/CalenderDateValidation.java
@@ -0,0 +1,36 @@
+package testBase.calender;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
+
+public class CalenderDateValidation {
+
+    public static void main(String[] args)throws InterruptedException {
+        System.setProperty("webdriver.chrome.driver",System.getProperty("user.dir")+"\\src\\drivers\\chromedriver.exe");
+        System.setProperty("webdriver.chrome.silentOutput","true");//it's prevent to showing unnecessary logs from the browser site in the console
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://fs2.formsite.com/R1Tuim/form1/index.html");
+        driver.manage().window().maximize();
+        Thread.sleep(3000);
+
+        driver.findElement(By.xpath("//*[@id=\"q6\"]/span")).click();
+        Select select = new Select(driver.findElement(By.xpath("//*[@id=\"ui-datepicker-div\"]/div/div/select")));
+        select.selectByVisibleText("2020");
+
+        for (int i=8;i>=1;i--){
+            Thread.sleep(1000);
+            driver.findElement(By.xpath("//*[@id=\"ui-datepicker-div\"]/div/a[2]/span")).click();
+            //get the month
+            String month = driver.findElement(By.xpath("//*[@id=\"ui-datepicker-div\"]/div/div/span")).getText();
+            if (month.equals("October")){
+                //in case dynamic date, we need to take link element
+                driver.findElement(By.linkText("20")).click();
+                break;//once we found expected month, we dont need to for further month
+            }
+        }
+
+        driver.close();
+    }
+}
--- src/testBase/table/ValidateColumnValueFromTable.java
+++ src/testBase/table/ValidateColumnValueFromTable.java
@@ -4,33 +4,44 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
-public class HowManyRowsColumnsInTable {
+import java.io.IOException;
 
-    public static void main(String[] args) throws InterruptedException{
+public class ValidateColumnValueFromTable {
+    public static void main(String[] args) throws InterruptedException {
         System.setProperty("webdriver.chrome.driver",System.getProperty("user.dir")+"\\src\\drivers\\chromedriver.exe");
         System.setProperty("webdriver.chrome.silentOutput","true");//it's prevent to showing unnecessary logs from the browser site in the console
         WebDriver driver = new ChromeDriver();
         driver.get("https://www.w3schools.com/html/html_tables.asp");
         driver.manage().window().maximize();
+        Thread.sleep(3000);
+
+
 
         int countRow = driver.findElements(By.xpath("//*[@id=\"main\"]/div[3]/div/table/tbody/tr")).size();
         System.out.println(countRow);
 
-        int colNums = driver.findElements(By.xpath("//*[@id=\"main\"]/div[3]/div/table/tbody/tr/td")).size();
-        System.out.println(colNums);
+        //initial status
+        int countryAvailable = 0;
 
         for (int i=2;i<=countRow;i++){
-            for (int j=1;j<=colNums;j++) {
-                System.out.println(driver.findElement(By.xpath("//*[@id=\"customers\"]/tbody/tr[" +i+ "]/td[" +j+ "]")).getText()+"  ");
-                //System.out.print(driver.findElement(By.xpath("//*[@id=\"main\"]/div[3]/div/table/tbody/tr["+i+"]/td["+j+"]")).getText());
+
+            System.out.println(i);
+
+            String status = driver.findElement(By.xpath("//*[@id=\"main\"]/div[3]/div/table/tbody/tr["+i+"]/td[3]")).getText();
+            //System.out.println(status);
+            if (status.equals("Germany")){
+                countryAvailable = countryAvailable+1;
                 break;
 
             }
         }
-        //System.out.println();
+        //System.out.println("Country doesn't matched");
+        //System.out.println("no employees is enabled"+statusCount);
         driver.close();
+
+
+
+
+
     }
-
-
-
 }
