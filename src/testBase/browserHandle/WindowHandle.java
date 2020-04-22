804f9cc88cbbdbcf6f55d7aa58a9bb27e1748228
sharif.ny <sharifaiub09@gmail.com>
2020-04-22 01:42:27 -0400

    Commit 1

---
+++ src/testBase/browserHandle/WindowHandle.java
@@ -0,0 +1,27 @@
+package testBase.browserHandle;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.Set;
+
+public class WindowHandle {
+    public static void main(String[] args) throws InterruptedException{
+        System.setProperty("webdriver.chrome.driver",System.getProperty("user.dir")+"\\src\\drivers\\chromedriver.exe");
+        System.setProperty("webdriver.chrome.silentOutput","true");//it's prevent to showing unnecessary logs from the browser site in the console
+        WebDriver driver = new ChromeDriver();
+        driver.get("http://demo.automationtesting.in/Windows.html");
+        driver.manage().window().maximize();
+
+        Thread.sleep(2000);
+        driver.findElement(By.xpath("//*[@id=\"Tabbed\"]/a/button")).click();
+
+        Set<String> elements = driver.getWindowHandles();
+
+        for (String key:elements){
+            String st = driver.switchTo().window(key).getTitle();
+            System.out.println(st);
+        }
+    }
+}
